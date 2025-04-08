a7b7737c995a2926d90d83baba47b28e95bdf329
Cesar Merino-Soto <sikayax@yahoo.com.ar>
2025-04-08 11:17:09 -0600

    Update CVRcut.R

--- CVRcut/CVRcut.R
+++ CVRcut/CVRcut.R
@@ -3,17 +3,20 @@
 #' Builds a table of critical values for Lawshe's Content Validity Ratio (CVR),
 #' using one of three methods: Wilson (confidence intervals), Ayres, or Baghestani (Bayesian).
 #'
+#' @name CVRcut
+#' @keywords validity CVR content-analysis
+#'
 #' @param N_min Integer. Minimum number of judges (must be < `N_max`).
 #' @param N_max Integer. Maximum number of judges.
 #' @param method Character. Method to compute the CVR cutoff values. Options are:
-#' `"Wilson"` (default), `"Ayres"`, or `"Bag"`.
+#' `"Wilson"`, `"Ayres"`, or `"Bag"`.
 #' @param alpha Numeric. Significance level (used only in `"Wilson"` and `"Bag"` methods). Default is 0.05.
 #' @param tails Character. Tail type for confidence interval calculation (only for `"Wilson"` method).
 #' Options are `"one"` or `"two"`. Default is `"one"`.
 #' @param prior Character. Prior distribution to use in the `"Bag"` method (e.g., `"Jeffreys"`). Default is `"Jeffreys"`.
 #' @param interpolacion Character. Reserved for future use. Default is `"none"`.
 #'
-#' @return A data frame containing the CVR cutoff values for each number of judges (`N`) in the range specified.
+#' @return A data frame with columns indicating the number of judges (`N`) and the corresponding CVR critical values.
 #' The structure of the output depends on the method used.
 #'
 #' @details
@@ -23,20 +26,15 @@
 #' - **Wilson method**: Calculates confidence intervals for proportions.
 #' - **Ayres method**: Uses the original proposal by Ayres (1972).
 #' - **Bag method**: Bayesian method proposed by Baghestani (1993), with prior selection.
-#' 
-#' @references
-#'Ayre, C., & Scally, A. J. (2014). Critical Values for Lawshe’s Content Validity 
-#'Ratio: Revisiting the Original Methods of Calculation. Measurement and Evaluation 
-#'in Counseling and Development, 47(1), 79–86. https://doi.org/10.1177/0748175613513808
 #'
-#' Baghestani, A. R., Ahmadi, F., Tanha, A., & Meshkat, M. (2017). Bayesian Critical Values 
-#' for Lawshe’s Content Validity Ratio. Measurement and Evaluation in Counseling and 
-#' Development, 52(1), 69–73. https://doi.org/10.1080/07481756.2017.1308227
-#' 
-#' Wilson, F. R., Pan, W., & Schumsky, D. A. (2012). Recalculation of the Critical Values
-#' for Lawshe’s Content Validity Ratio. Measurement and Evaluation in Counseling 
-#' and Development, 45(3), 197–210. https://doi.org/10.1177/0748175612440286
-
+#' @references
+#' Lawshe, C. H. (1975). A quantitative approach to content validity. *Personnel Psychology*, 28(4), 563–575. https://doi.org/10.1111/j.1744-6570.1975.tb01393.x
+#'
+#' Ayre, C., & Scally, A. J. (2014). Critical Values for Lawshe’s Content Validity Ratio: Revisiting the Original Methods of Calculation. *Measurement and Evaluation in Counseling and Development*, 47(1), 79–86. https://doi.org/10.1177/0748175613513808
+#'
+#' Baghestani, A. R., Ahmadi, F., Tanha, A., & Meshkat, M. (2017). Bayesian Critical Values for Lawshe’s Content Validity Ratio. *Measurement and Evaluation in Counseling and Development*, 52(1), 69–73. https://doi.org/10.1080/07481756.2017.1308227
+#'
+#' Wilson, F. R., Pan, W., & Schumsky, D. A. (2012). Recalculation of the Critical Values for Lawshe’s Content Validity Ratio. *Measurement and Evaluation in Counseling and Development*, 45(3), 197–210. https://doi.org/10.1177/0748175612440286
 #'
 #' @seealso [CVRcut.Wilson()], [CVRcut.Ayres()], [CVRcut.Bag()]
 #'
